b91fea8e69935608760920e7defd2e9dd9a98649
Abdelaziz HAIMOUD <abdelazizhaimoud@outlook.com>
2026-03-11 00:56:08 +0000

    Add Activity model, controller, and migration files for activities and activity_user

---
+++ app/Models/Activity.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Activity extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "title",
+        "category",
+        "location",
+        "date_time",
+        "max_participants",
+        "host_id",
+    ];
+}
