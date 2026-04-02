c644e9cc40f840e4e891ec40e7d40d1b4ebcfe65
Abdelaziz HAIMOUD <abdelazizhaimoud@outlook.com>
2026-04-02 02:42:53 +0100

    Update UserFactory to use specific user data; modify activities migration to change category field type and update DatabaseSeeder to create a user with default values

--- database/migrations/2026_03_11_004459_create_activities_table.php
+++ database/migrations/2026_03_11_004459_create_activities_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('activities', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('category');
+            $table->enum('category',['sport','outdoor','travel']);
             $table->string('location');
             $table->date('date_time');
             $table->integer('max_participants');
