e3d646209393e2b174a5f4227f9010e20820ce6c
Abdelaziz HAIMOUD <abdelazizhaimoud@outlook.com>
2026-04-17 00:03:11 +0100

    Add avatar upload functionality to UserController and create .gitignore for storage

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,12 @@ class UserController extends Controller
             "name" => "nullable|string|min:2",
             "email" => "nullable|string|email",
             "bio" => "nullable|string|min:8",
+            "avatar" => "nullable|file|image|mimes:jpeg,png,jpg,gif,svg|max:2048"
         ]);
+        if($request->hasFile('avatar')){
+            $avatarPath = $request->file('avatar')->store('avatars', 'public');
+            $validated['avatar'] = $avatarPath;
+        }
 
         $user->update($validated);
         return response()->json([
